fa8280d6b956c86793ae5b5371981ce370ed1461
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-10-25 22:23:36 +0700

    edit

--- components/profile/profile-card.tsx
+++ components/profile/profile-card.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Profile } from "./types";
+import Image from "next/image";
 
 function ProfileCard({ profile }: { profile: Profile }) {
+  const imageSrc = profile.image || "";
   return (
     <div className="border border-solid border-darkgray2 p-2 tbt:h-1/2 tbt:mr-4 tbt:w-1/2 md:w-2/5 lg:w-1/4 2xl:w-1/5">
       <div>
         <p className="text-center text-sm">{profile.username || "unknown"}</p>
         <p className="text-center text-sm mb-2">{profile.email || "email"}</p>
         <div className="grid place-items-center mb-2 w-full h-25">
-          <img className="rounded-full" width="100" height="100" src={profile.image} alt={profile.username} />
+          <Image className="rounded-full" width="100" height="100" src={imageSrc} alt={profile.username ?? "profile"} />
         </div>
       </div>
       <div className="flex justify-center items-center">
